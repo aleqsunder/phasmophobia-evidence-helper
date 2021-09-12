9af0b8b5e849bcd7e60171eb898885a2822490f6
aleqsunder <al.eq@outlook.com>
2021-09-12 22:28:14 +0400

    Рефакторинг GHOSTS массива, теперь он занимает меньше места

--- src/js/component/app.vue
+++ src/js/component/app.vue
@@ -15,10 +15,10 @@
     <div class="item">
         <h3 class="item__title">Призраки</h3>
         <ul class="item__content" data-ghosts>
-            <li class="item__content-li" v-for="(item, key) in filteredGhosts" :key="key">
-                <p class="item__content-li__header">{{ GHOSTS[item].name }}</p>
+            <li class="item__content-li" v-for="item in filteredGhosts">
+                <p class="item__content-li__header">{{ item }}</p>
                 <ul v-if="SELECTED.EVIDENCES.length === 2" class="item__content-li__massive">
-                    <template v-for="evidence in GHOSTS[item].evidences">
+                    <template v-for="evidence in GHOSTS[item]">
                         <li v-if="!SELECTED.EVIDENCES.includes(evidence)" class="item__content-li__massive-item">
                             {{ EVIDENCE[evidence] }}
                         </li>
@@ -50,70 +50,22 @@
                 },
 
                 GHOSTS: {
-                    SPIRIT: {
-                        name: 'Дух',
-                        evidences: ['EMP', 'RADIO', 'BOOK'],
-                    },
-                    WRAITH: {
-                        name: 'Мираж',
-                        evidences: ['EMP', 'LASER', 'RADIO'],
-                    },
-                    PHANTOM: {
-                        name: 'Фантом',
-                        evidences: ['RADIO', 'FINGER', 'LASER'],
-                    },
-                    POLTERGEIST: {
-                        name: 'Полтергейст',
-                        evidences: ['RADIO', 'FINGER', 'BOOK'],
-                    },
-                    BANSHEE: {
-                        name: 'Банши',
-                        evidences: ['LASER', 'FINGER', 'ORB'],
-                    },
-                    JINN: {
-                        name: 'Джинн',
-                        evidences: ['TEMP', 'FINGER', 'EMP'],
-                    },
-                    MARE: {
-                        name: 'Мара',
-                        evidences: ['ORB', 'RADIO', 'BOOK'],
-                    },
-                    REVENANT: {
-                        name: 'Ревенант',
-                        evidences: ['ORB', 'TEMP', 'BOOK'],
-                    },
-                    SHADE: {
-                        name: 'Тень',
-                        evidences: ['BOOK', 'EMP', 'TEMP'],
-                    },
-                    DEMON: {
-                        name: 'Демон',
-                        evidences: ['FINGER', 'BOOK', 'TEMP'],
-                    },
-                    YUREI: {
-                        name: 'Юрэй',
-                        evidences: ['ORB', 'LASER', 'TEMP'],
-                    },
-                    ONI: {
-                        name: 'Они',
-                        evidences: ['TEMP', 'EMP', 'LASER'],
-                    },
-                    HANTU: {
-                        name: 'Ханту',
-                        evidences: ['FINGER', 'ORB', 'TEMP'],
-                    },
-                    YOKAI: {
-                        name: 'Ёкай',
-                        evidences: ['RADIO', 'ORB', 'LASER'],
-                    },
-                    GORYO: {
-                        name: 'Горё',
-                        evidences: ['EMP', 'FINGER', 'LASER'],
-                    },
-                    MYLING: {
-                        name: 'Мюлинг',
-                        evidences: ['EMP', 'FINGER', 'BOOK'],
-                    }
+                    'Дух': ['EMP', 'RADIO', 'BOOK'],
+                    'Мираж': ['EMP', 'LASER', 'RADIO'],
+                    'Фантом': ['RADIO', 'FINGER', 'LASER'],
+                    'Полтергейст': ['RADIO', 'FINGER', 'BOOK'],
+                    'Банши': ['LASER', 'FINGER', 'ORB'],
+                    'Джинн': ['TEMP', 'FINGER', 'EMP'],
+                    'Мара': ['ORB', 'RADIO', 'BOOK'],
+                    'Ревенант': ['ORB', 'TEMP', 'BOOK'],
+                    'Тень': ['BOOK', 'EMP', 'TEMP'],
+                    'Демон': ['FINGER', 'BOOK', 'TEMP'],
+                    'Юрэй': ['ORB', 'LASER', 'TEMP'],
+                    'Они': ['TEMP', 'EMP', 'LASER'],
+                    'Ханту': ['FINGER', 'ORB', 'TEMP'],
+                    'Ёкай': ['RADIO', 'ORB', 'LASER'],
+                    'Горё': ['EMP', 'FINGER', 'LASER'],
+                    'Мюлинг': ['EMP', 'FINGER', 'BOOK'],
                 }
             }
         },
@@ -133,7 +85,7 @@
         computed: {
             filteredGhosts () {
                 return Object.keys(this.GHOSTS).filter(ghost => {
-                    return this.SELECTED.EVIDENCES.every(evidence => this.GHOSTS[ghost].evidences.includes(evidence))
+                    return this.SELECTED.EVIDENCES.every(evidence => this.GHOSTS[ghost].includes(evidence))
                 })
             },
 
