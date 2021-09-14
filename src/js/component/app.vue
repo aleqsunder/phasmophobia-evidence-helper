fd5ac2f6fadb2b82242c9327e08eafd966dacf7e
aleqsunder <al.eq@outlook.com>
2021-09-14 22:12:21 +0400

    Добавил уведомления и возможность заполнения улик при нажатии на призрака

--- src/js/component/app.vue
+++ src/js/component/app.vue
@@ -1,4 +1,7 @@
 <template>
+    <div class="notifications">
+        <notification v-for="notification in notifications" :key="notification.key" :notification="notification" @delete="removeNotification"/>
+    </div>
     <div class="item">
         <h3 class="item__title">Выбранные улики</h3>
         <transition-group v-if="SELECTED.EVIDENCES.length > 0" name="item__content" tag="ul" class="item__content">
@@ -15,7 +18,7 @@
     <div class="item">
         <h3 class="item__title">Призраки</h3>
         <ul class="item__content" data-ghosts>
-            <li class="item__content-li" v-for="item in filteredGhosts">
+            <li class="item__content-li" v-for="item in filteredGhosts" @click="showGhostEvidences(GHOSTS[item])">
                 <p class="item__content-li__header">{{ item }}</p>
                 <ul v-if="SELECTED.EVIDENCES.length === 2" class="item__content-li__massive">
                     <template v-for="evidence in GHOSTS[item]">
@@ -31,12 +34,16 @@
 
 <script>
     import { ghosts, evidences } from '../constants/lang/ru/list'
+    import notification from "./notification";
 
     export default {
         name: "app",
+        components: {notification},
 
         data () {
             return {
+                notifications: [],
+
                 SELECTED: {
                     EVIDENCES: [],
                 },
@@ -77,10 +84,29 @@
                 if (this.SELECTED.EVIDENCES.length < 3 && !this.SELECTED.EVIDENCES.includes(key)) {
                     this.SELECTED.EVIDENCES = [...this.SELECTED.EVIDENCES, key]
                 }
+
+                if (this.filteredGhosts.length === 0) {
+                    this.unselect(key)
+                    this.addNotification(`"${this.EVIDENCE[key]}": добавление данного пункта приведёт к пустому списку призраков`)
+                }
             },
 
             unselect (key) {
                 this.SELECTED.EVIDENCES = this.SELECTED.EVIDENCES.filter(e => e !== key)
+            },
+
+            showGhostEvidences (evidences) {
+                this.SELECTED.EVIDENCES = evidences
+            },
+
+            addNotification (message) {
+                this.notifications = [...this.notifications, {key: Date.now() / 1000 ^ 0, message}]
+            },
+
+            removeNotification (key) {
+                this.notifications = this.notifications.filter(notification => {
+                    return notification.key !== key
+                })
             }
         },
 
---
+++ src/js/component/notification.vue
@@ -0,0 +1,22 @@
+<template>
+    <p class="notifications__item">
+        {{ this.notification.message }}
+    </p>
+</template>
+
+<script>
+    export default {
+        name: "notification",
+        props: ['notification'],
+
+        methods: {
+            deleteNotification () {
+                this.$emit('delete', this.notification.key)
+            }
+        },
+
+        created () {
+            setTimeout(this.deleteNotification, 2200)
+        }
+    }
+</script>
